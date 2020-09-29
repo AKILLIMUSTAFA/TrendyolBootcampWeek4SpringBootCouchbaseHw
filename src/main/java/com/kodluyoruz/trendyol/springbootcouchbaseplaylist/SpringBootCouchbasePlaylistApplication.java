8f1a6fe58e10469376d2f736becf33add308028f
Mustafa Akıllı <makilli@gtu.edu.tr>
2020-09-29 07:11:47 +0300

    The project has been created and added swagger

---
+++ src/main/java/com/kodluyoruz/trendyol/springbootcouchbaseplaylist/SpringBootCouchbasePlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.kodluyoruz.trendyol.springbootcouchbaseplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCouchbasePlaylistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCouchbasePlaylistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodluyoruz/trendyol/springbootcouchbaseplaylist/SpringBootCouchbasePlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodluyoruz.trendyol.springbootcouchbaseplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCouchbasePlaylistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
